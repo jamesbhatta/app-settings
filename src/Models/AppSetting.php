6fd0d7c1b54d1a092d2df0293628097c0d3e4193
manojbhatta <jmsbhatta@gmail.com>
2020-12-24 01:26:15 +0545

    wip

--- src/Models/AppSetting.php
+++ src/Models/AppSetting.php
@@ -3,6 +3,7 @@
 namespace JamesBhatta\AppSettings\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Cache;
 
 class AppSetting extends Model
 {
@@ -10,18 +11,77 @@ class AppSetting extends Model
 
     protected $table = 'app_settings';
 
+    protected $cacheKey = 'jb-app-settings';
+
     public function set($key, $value = null)
     {
-        $this->getAppSettingModel()->firstOrCreate([
-            'key' => $key,
-            'value' => $value
+        if (is_array($key)) {
+            foreach ($key as $k => $v) {
+                $this->set($k, $v);
+            }
+            return true;
+        }
+
+        $appSetting = $this->getAppSettingModel()->firstOrnew([
+            'key' => $key
         ]);
 
+        $appSetting->key = $key;
+        $appSetting->value = $value;
+        $appSetting->save();
+
+        $this->flushCache();
+
         return $value;
+    }
+
+    public function allSettings($fresh = false)
+    {
+        if ($fresh) {
+            return $this->getModelQuery()->pluck('value', 'key');
+        }
+
+        return Cache::rememberForever($this->getCacheKey(), function () {
+            return $this->getModelQuery()->pluck('value', 'key');
+        });
+    }
+
+    public function get($key, $default = null, $fresh = false)
+    {
+        return $this->allSettings($fresh)->get($key, $default);
+    }
+
+    public function has($key)
+    {
+        return $this->allSettings()->has($key);
+    }
+
+    public function remove($key)
+    {
+        $removed = $this->getAppSettingModel()->where('key', $key)->delete();
+        
+        $this->flushCache();
+
+        return $removed;
+    }
+
+    public function flushCache()
+    {
+        return Cache::forget($this->getCacheKey());
     }
 
     public function getAppSettingModel()
     {
         return app('\JamesBhatta\AppSettings\Models\AppSetting');
+    }
+
+    public function getModelQuery()
+    {
+        return $this->getAppSettingModel();
+    }
+
+    public function getCacheKey()
+    {
+        return $this->cacheKey;
     }
 }
--- tests/AppSettingsTest.php
+++ tests/AppSettingsTest.php
@@ -3,6 +3,7 @@
 namespace JamesBhatta\AppSettings\Tests;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Cache;
 use JamesBhatta\AppSettings\Models\AppSetting;
 
 class AppSettingsTests extends TestCase
@@ -31,8 +32,62 @@ class AppSettingsTests extends TestCase
     {
         $this->appSettings->set('name', 'James Bhatta');
         $this->assertDatabaseHas('app_settings', ['key' => 'name', 'value' => 'James Bhatta']);
-     
+
         $this->appSettings->set('name', 'James');
         $this->assertDatabaseHas('app_settings', ['key' => 'name', 'value' => 'James']);
+        $this->assertDatabaseMissing('app_settings', ['key' => 'name', 'value' => 'James Bhatta']);
+    }
+
+    /** @test */
+    public function it_can_get_setting_by_key()
+    {
+        $this->appSettings->set('name', 'James Bhatta');
+
+        $this->assertEquals('James Bhatta', $this->appSettings->get('name'));
+    }
+
+    /** @test */
+    public function it_stores_array_as_individual_key_value_pairs()
+    {
+        $this->appSettings->set([
+            'name' => 'James Bhatta',
+            'profession' => 'programmer',
+            'website' => 'manojbhatta.com.np'
+        ]);
+
+        $this->assertCount(3, $this->appSettings->allSettings());
+        $this->assertEquals('James Bhatta', $this->appSettings->get('name', null, $fresh = true));
+        $this->assertEquals('programmer', $this->appSettings->get('profession'));
+        $this->assertEquals('manojbhatta.com.np', $this->appSettings->get('website'));
+    }
+
+    /** @test */
+    public function it_can_check_of_certail_key_exists()
+    {
+        $this->appSettings->set('name', 'James Bhatta');
+
+        $this->assertTrue($this->appSettings->has('name'));
+        $this->assertFalse($this->appSettings->has('unknown_key'));
+    }
+
+    /** @test */
+    public function it_caches_settings()
+    {
+        $this->appSettings->set('name', 'James Bhatta');
+
+        Cache::shouldReceive('rememberForever')
+            ->once();
+
+        $this->appSettings->allsettings();
+    }
+
+    /** @test */
+    public function it_can_remove_a_key_from_setting()
+    {
+        $this->appSettings->set('name', 'James Bhatta');
+        
+        $this->appSettings->remove('name');
+
+        $this->assertCount(0, $this->appSettings->allSettings());
     }
 }
