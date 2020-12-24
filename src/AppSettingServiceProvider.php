754193f9845cf0fb3fbd6a198dfbfb08fa5cd871
manojbhatta <jmsbhatta@gmail.com>
2020-12-24 14:10:15 +0545

    wip

--- src/AppSettingServiceProvider.php
+++ src/AppSettingServiceProvider.php
@@ -9,16 +9,27 @@ class AppSettingServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        // 
+        $this->publishMigrations();
     }
 
     public function register()
     {
-        $this->app->bind('setting', function($app) {
+        $this->app->bind('setting', function ($app) {
             return new Setting();
         });
 
         // load migrations
         $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+    }
+
+    protected function publishMigrations()
+    {
+        if ($this->app->runningInConsole()) {
+            if (!class_exists('CreateAppSettingsTable')) {
+                $this->publishes([
+                    __DIR__ . '/../database/migrations/create_app_settings.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . 'create_app_settings.php'),
+                ], 'migrations');
+            }
+        }
     }
 }
